96d15fc1f07eae906749a789144bc24dd1d11678
moritz.brantner <moritz.brantner@gmail.com>
2026-08-28 20:32:22 +0200

    Resolve resource list exports after reorder merge

--- src/components/data/resource-list.tsx
+++ src/components/data/resource-list.tsx
@@ -247,6 +247,5 @@ export type ResourceListHeaderProps = React.ComponentProps<typeof ResourceListHe
 export type ResourceListResetButtonProps = React.ComponentProps<typeof ResourceListResetButton>;
 export type ResourceListToolbarProps = React.ComponentProps<typeof ResourceListToolbar>;
 
-<<<<<<< HEAD
 export * from "./resource-list/refresh-control";
 export * from "./resource-list/reorderable-list";
