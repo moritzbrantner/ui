f91bd8db5e7ba9e5bad7fdb35ce37c5ee850bc2e
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-08-28 08:54:57 +0200

    Export refresh control from data API

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,7 @@
 
 export * from "./components/data/data-grid";
 export * from "./components/data/filter-bar";
+export * from "./components/data/refresh-control";
 export * from "./components/data/resource-list";
 export * from "./components/data/search-field";
 export * from "./components/data/selection-toolbar";
