50fd530d496da8487fe928bd4d9c7f10d41e01b4
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-08-28 08:57:25 +0200

    Export reorderable list from data API

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,7 @@
 
 export * from "./components/data/data-grid";
 export * from "./components/data/filter-bar";
+export * from "./components/data/reorderable-list";
 export * from "./components/data/resource-list";
 export * from "./components/data/search-field";
 export * from "./components/data/selection-toolbar";
