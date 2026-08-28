4f29a9387960731ca335a71f36869157193b8699
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-08-28 09:08:07 +0200

    Keep reorderables under registered resource list API

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,6 @@
 
 export * from "./components/data/data-grid";
 export * from "./components/data/filter-bar";
-export * from "./components/data/reorderable-list";
 export * from "./components/data/resource-list";
 export * from "./components/data/search-field";
 export * from "./components/data/selection-toolbar";
